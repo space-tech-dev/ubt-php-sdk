a2cd2ff5e65ed88138896c1aece95dedf03ffc89
Walle <nomail>
2020-11-30 12:35:32 +0800

    增加RabbitMQ Exchange输出到Queue，避免logstash实例出错的时候丢失消息。

--- src/UBT.php
+++ src/UBT.php
@@ -17,6 +17,14 @@ class UBT
     protected static $channel = "log4php";
     private static $logger;
     protected static $baseParams = [];
+    /**
+     * @var LineFormatter
+     */
+    private static $formatter;
+    /**
+     * @var int
+     */
+    private static $LOG_LEVEL;
 
     public function __construct()
     {
@@ -26,42 +34,82 @@ class UBT
                 'appName' => env('UBT_APP_NAME', env('APP_NAME', 'app')),
                 'appVersion' => env('UBT_APP_VERSION', env('APP_VERSION', 'app')),
                 'serverHostname' => gethostname(),
-                'serverAddr' => $this->getIpAddress(),
+                'serverAddr' => Utils::getClientIpAddress(),
             ];
 
-            $LOG_LEVEL = $this->formatLogLevel(env('UBT_LOG_LEVEL', 'DEBUG'));
+            self::$LOG_LEVEL = Utils::formatLogLevel(env('UBT_LOG_LEVEL', 'DEBUG'));
+
             // the default date format is "Y-m-d\TH:i:sP"
             $dateFormat = "c";
             // the default output format is "[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n"
             $output = "%datetime% %level_name% %message%\n";
             // finally, create a formatter
-            $formatter = new LineFormatter($output, $dateFormat);
+            self::$formatter = new LineFormatter($output, $dateFormat);
 
-            self::$logger = $logger = new Logger('logger');
+            self::$logger = new Logger('logger');
 
-            // 发送到文件
-            $streamHandler = new RotatingFileHandler(storage_path() . '/logs/ubt-redis.log', 7, $LOG_LEVEL);
-            $streamHandler->setFormatter($formatter);
-            $logger->pushHandler($streamHandler);
+            $this->initFileStream();
 
             // 发送到redis
             if (env('UBT_REDIS')) {
-                $redisHandler = new RedisHandler(new Client(env('UBT_REDIS')), "ubt-logs", $LOG_LEVEL);
-                $redisHandler->setFormatter($formatter);
-                $logger->pushHandler($redisHandler);
+                $this->initRedisStream();
             } else if (env('UBT_AMQP_URL')) {
-                // 发送到Rabbit MQ
-                $url = parse_url(getenv('UBT_AMQP_URL'));
-                $connection = new AMQPStreamConnection($url['host'], $url['port'], $url['user'], $url['pass'], substr($url['path'], 1));
-                $channel = $connection->channel();
-
-                $channel->exchange_declare(env('UBT_AMQP_EXCHANGE', 'logs'), 'direct', false, true, false); //声明初始化交换机
-                $mqHandler = new AmqpHandler($channel, env('UBT_AMQP_EXCHANGE', 'logs'), $LOG_LEVEL);
-                $mqHandler->setFormatter($formatter);
-                $logger->pushHandler($mqHandler);
+                $this->initAMQPStream();
             }
 
         }
+    }
+
+    /**
+     * 存储到本地文件系统。这个只用来调试，不会发往es
+     */
+    protected function initFileStream() {
+        // 发送到文件
+        $streamHandler = new RotatingFileHandler(storage_path() . '/logs/ubt-redis.log', 7, self::$LOG_LEVEL);
+        $streamHandler->setFormatter(self::$formatter);
+        self::$logger->pushHandler($streamHandler);
+    }
+
+    /**
+     * 存储到Redis
+     */
+    protected function initRedisStream() {
+        $redisHandler = new RedisHandler(new Client(env('UBT_REDIS')), "ubt-logs", self::$LOG_LEVEL);
+        $redisHandler->setFormatter(self::$formatter);
+        self::$logger->pushHandler($redisHandler);
+    }
+
+    /**
+     * 存储到RabbitMQ
+     */
+    protected function initAMQPStream() {
+        $exchangeName = env('UBT_AMQP_EXCHANGE', 'exchange-ubt-logs');
+        $queueName = env('UBT_AMQP_QUEUE', 'queue-ubt-logs');
+
+        $url = parse_url(getenv('UBT_AMQP_URL'));
+        $connection = new AMQPStreamConnection($url['host'], $url['port'], $url['user'], $url['pass'], substr($url['path'], 1));
+        $channel = $connection->channel();
+
+        $channel->exchange_declare($exchangeName, 'direct', false, true, false); //声明初始化交换机
+        $channel->queue_declare($queueName, false, true, false, false);
+
+        $msgTypeArr = [
+            'debug.logger',
+            'info.logger',
+            'error.logger',
+            'warning.logger',
+            'notice.logger',
+            'critical.logger',
+            'alert.logger',
+            'emergency.logger',
+        ];
+        foreach ($msgTypeArr as $msgType) {
+            $channel->queue_bind($queueName, $exchangeName, $msgType);
+        }
+
+        $mqHandler = new AmqpHandler($channel, $exchangeName, self::$LOG_LEVEL);
+        $mqHandler->setFormatter(self::$formatter);
+        self::$logger->pushHandler($mqHandler);
     }
 
     static function setBaseParams($params = [])
@@ -124,62 +172,11 @@ class UBT
         ]);
     }
 
-    protected function formatLogLevel($logLevel = "debug") {
-        switch (mb_strtolower($logLevel)) {
-            case 'info':
-                return Logger::INFO;
-            case 'notice':
-                return Logger::NOTICE;
-            case 'waring':
-                return Logger::WARNING;
-            case 'error':
-                return Logger::ERROR;
-            case 'critical':
-                return Logger::CRITICAL;
-            case 'alert':
-                return Logger::ALERT;
-            default:
-                return Logger::DEBUG;
-        }
-    }
-
-    protected function formatMsg($msg, $json = [])
-    {
-        // 如果msg不是字符串，那么只会接受一个msg，
-        if (gettype($msg) !== 'string') {
-            try {
-                $formatData = json_encode(array_merge(self::$baseParams, $msg));
-            } catch (Exception $e) {
-                if (Utils::isNoEnvProduction()) {
-                    throw $e;
-                }
-                return '';
-            }
-        } else {
-            $formatData = json_encode(array_merge(self::$baseParams, [
-                "msg" => $msg,
-//                "logLevel" => strtoupper($logLevel)
-            ], $json));
-        }
-
-        return $formatData;
-    }
-
     private function base($logLevel, $msg, $json = [])
     {
         try {
-            $formatData = $this->formatMsg($msg, $json);
+            $formatData = Utils::formatMsg(self::$baseParams, $msg, $json);
             self::$logger->{$logLevel}($formatData);
         } catch (\Throwable $e) {}
-    }
-
-    private function getIpAddress() {
-        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $ipAddresses = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-            return trim(end($ipAddresses));
-        }
-        else {
-            return isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '';
-        }
     }
 }
--- src/Utils.php
+++ src/Utils.php
@@ -4,6 +4,8 @@
 namespace ArtisanCloud\UBT;
 
 
+use Monolog\Logger;
+
 class Utils
 {
     static function isNoEnvProduction() {
@@ -12,5 +14,55 @@ class Utils
             env('APP_ENV') === 'development' ||
             env('APP_ENV') === 'test' ||
             env('APP_ENV') === 'staging';
+    }
+
+    static function formatLogLevel($logLevel = "debug") {
+        switch (mb_strtolower($logLevel)) {
+            case 'info':
+                return Logger::INFO;
+            case 'notice':
+                return Logger::NOTICE;
+            case 'waring':
+                return Logger::WARNING;
+            case 'error':
+                return Logger::ERROR;
+            case 'critical':
+                return Logger::CRITICAL;
+            case 'alert':
+                return Logger::ALERT;
+            default:
+                return Logger::DEBUG;
+        }
+    }
+
+    static function getClientIpAddress() {
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ipAddresses = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            return trim(end($ipAddresses));
+        }
+        else {
+            return isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '';
+        }
+    }
+
+    static function formatMsg($baseParams, $msg, $json = [])
+    {
+        // 如果msg不是字符串，那么只会接受一个msg，
+        if (gettype($msg) !== 'string') {
+            try {
+                $formatData = json_encode(array_merge($baseParams, $msg));
+            } catch (\Exception $e) {
+                if (Utils::isNoEnvProduction()) {
+                    throw $e;
+                }
+                return '';
+            }
+        } else {
+            $formatData = json_encode(array_merge($baseParams, [
+                "msg" => $msg,
+            ], $json));
+        }
+
+        return $formatData;
     }
 }
